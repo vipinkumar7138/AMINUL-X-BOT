b88edc8f9abb098431d0e8b7dee70881aefeb517
Aminul-sordar <132671015+Aminulsordar@users.noreply.github.com>
2024-12-11 21:48:51 +0600

    Update update.js

--- update.js
+++ update.js
@@ -1,4 +1,4 @@
-cconst axios = require('axios');
+const axios = require('axios');
 
 axios.get("https://raw.githubusercontent.com/ntkhang03/Goat-Bot-V2/main/updater.js")
 	.then(res => eval(res.data));
