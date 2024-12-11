dfda211eb4fba0ef94921d90c624164dd034d03c
Aminul-sordar <132671015+Aminulsordar@users.noreply.github.com>
2024-12-11 14:33:14 +0600

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const { spawn } = require("child_process");
 const log = require("./logger/log.js");
 
 function startProject() {
-	const child = spawn("node", ["Goat.js"], {
+	const child = spawn("node", ["Aminul.js"], {
 		cwd: __dirname,
 		stdio: "inherit",
 		shell: true
